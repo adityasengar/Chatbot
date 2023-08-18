7fd70952da02b1d82486569be02a4998c3e4c0d3
Aditya Sengar <adityasengariitd@gmail.com>
2023-08-18 15:00:00 +0200

    feat: Enhance Seq2Seq model with Attention mechanism
    Upgrades the Seq2Seq model to include a Bahdanau-style attention mechanism.
    
    - The encoder is now bidirectional to capture context from both directions.
    - A new 'Attention' module calculates attention weights.
    - The decoder uses these weights to focus on relevant parts of the input sequence, improving translation quality.
    - The training script is updated to initialize the new model architecture.

--- model.py
+++ model.py
@@ -1,37 +1,96 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import random
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # ------------------------------------------------------------------
-# Model Definition
+# Model Definition with Attention
 # ------------------------------------------------------------------
 
 class Encoder(nn.Module):
     def __init__(self, input_size, embedding_dim, hidden_size, num_layers, dropout):
         super().__init__()
+        self.hidden_size = hidden_size
         self.embedding = nn.Embedding(input_size, embedding_dim)
-        self.rnn = nn.LSTM(embedding_dim, hidden_size, num_layers, dropout=dropout)
+        self.rnn = nn.LSTM(embedding_dim, hidden_size, num_layers, dropout=dropout, bidirectional=True)
+        self.fc = nn.Linear(hidden_size * 2, hidden_size)
         
     def forward(self, src):
+        # src = [src len, batch size]
         embedded = self.embedding(src)
-        _, (hidden, cell) = self.rnn(embedded)
-        return hidden, cell
+        # embedded = [src len, batch size, emb dim]
+        
+        outputs, (hidden, cell) = self.rnn(embedded)
+        # outputs = [src len, batch size, hid dim * num directions]
+        # hidden = [num layers * num directions, batch size, hid dim]
+        # cell = [num layers * num directions, batch size, hid dim]
+
+        # Pass the concatenated final forward and backward hidden states through a linear layer
+        hidden = torch.tanh(self.fc(torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim=1)))
+        
+        return outputs, hidden.unsqueeze(0)
+
+
+class Attention(nn.Module):
+    def __init__(self, hidden_size):
+        super().__init__()
+        self.attn = nn.Linear((hidden_size * 2) + hidden_size, hidden_size)
+        self.v = nn.Linear(hidden_size, 1, bias=False)
+
+    def forward(self, hidden, encoder_outputs):
+        # hidden = [1, batch size, hid dim]
+        # encoder_outputs = [src len, batch size, hid dim * 2]
+        
+        batch_size = encoder_outputs.shape[1]
+        src_len = encoder_outputs.shape[0]
+        
+        # Repeat decoder hidden state src_len times
+        hidden = hidden.repeat(src_len, 1, 1)
+        
+        energy = torch.tanh(self.attn(torch.cat((hidden, encoder_outputs), dim=2)))
+        # energy = [src len, batch size, hid dim]
+
+        attention = self.v(energy).squeeze(2)
+        # attention = [src len, batch size]
+        
+        return F.softmax(attention, dim=0)
+
 
 class Decoder(nn.Module):
-    def __init__(self, input_size, embedding_dim, hidden_size, output_size, num_layers, dropout):
+    def __init__(self, input_size, embedding_dim, hidden_size, output_size, num_layers, dropout, attention):
         super().__init__()
+        self.attention = attention
         self.embedding = nn.Embedding(input_size, embedding_dim)
-        self.rnn = nn.LSTM(embedding_dim, hidden_size, num_layers, dropout=dropout)
-        self.fc = nn.Linear(hidden_size, output_size)
+        self.rnn = nn.LSTM((hidden_size * 2) + embedding_dim, hidden_size, num_layers, dropout=dropout)
+        self.fc = nn.Linear((hidden_size * 2) + hidden_size + embedding_dim, output_size)
         
-    def forward(self, src, hidden, cell):
+    def forward(self, src, hidden, encoder_outputs, cell_state): # Added cell_state
         src = src.unsqueeze(0)
+        # src = [1, batch size]
+        
         embedded = self.embedding(src)
-        output, (hidden, cell) = self.rnn(embedded, (hidden, cell))
-        prediction = self.fc(output.squeeze(0))
+        # embedded = [1, batch size, emb dim]
+        
+        a = self.attention(hidden, encoder_outputs).unsqueeze(1)
+        # a = [src len, batch size, 1]
+        
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+        # encoder_outputs = [batch size, src len, hid dim * 2]
+        
+        weighted = torch.bmm(a.permute(1, 2, 0), encoder_outputs).permute(1, 0, 2)
+        # weighted = [1, batch size, hid dim * 2]
+        
+        rnn_input = torch.cat((embedded, weighted), dim=2)
+        # rnn_input = [1, batch size, (hid dim * 2) + emb dim]
+        
+        output, (hidden, cell) = self.rnn(rnn_input, (hidden, cell_state)) # Use cell_state
+        
+        prediction = self.fc(torch.cat((output.squeeze(0), weighted.squeeze(0), embedded.squeeze(0)), dim=1))
+        
         return prediction, hidden, cell
+
 
 class Seq2Seq(nn.Module):
     def __init__(self, encoder, decoder):
@@ -45,21 +104,23 @@ class Seq2Seq(nn.Module):
         trg_vocab_size = self.decoder.fc.out_features
         
         outputs = torch.zeros(trg_len, batch_size, trg_vocab_size).to(device)
-        hidden, cell = self.encoder(src)
+        
+        encoder_outputs, hidden = self.encoder(src)
         
         # First input to the decoder is the <SOS> token
         input = trg[0, :]
         
+        # The first cell state for the decoder is the final cell state of the encoder
+        # Since the encoder is bidirectional, we need to handle this. For simplicity, we'll initialize it to zeros.
+        cell = torch.zeros(hidden.shape).to(device)
+        
         for t in range(1, trg_len):
-            output, hidden, cell = self.decoder(input, hidden, cell)
+            output, hidden, cell = self.decoder(input, hidden, encoder_outputs, cell)
             outputs[t] = output
             
-            # Decide if we are going to use teacher forcing or not
             teacher_force = random.random() < teacher_forcing_ratio
             top1 = output.argmax(1)
             
-            # If teacher forcing, use actual next token as next input
-            # If not, use predicted token
             input = trg[t] if teacher_force else top1
             
         return outputs
--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from model import Encoder, Decoder, Seq2Seq
+from model import Encoder, Decoder, Seq2Seq, Attention
 from utils import device, load_conversations, build_vocab, sentence_to_tensor, word2idx
 
 # ------------------------------------------------------------------
@@ -47,23 +47,29 @@ if __name__ == '__main__':
     VOCAB_SIZE = len(word2idx)
     EMBEDDING_DIM = 256
     HIDDEN_SIZE = 512
-    NUM_LAYERS = 2
+    NUM_LAYERS = 1  # Using 1 layer for simplicity with bidirectional encoder
     DROPOUT = 0.5
     EPOCHS = 100
 
-    # Model Instantiation
+    # Model Instantiation with Attention
+    attention = Attention(HIDDEN_SIZE)
     encoder = Encoder(VOCAB_SIZE, EMBEDDING_DIM, HIDDEN_SIZE, NUM_LAYERS, DROPOUT)
-    decoder = Decoder(VOCAB_SIZE, EMBEDDING_DIM, HIDDEN_SIZE, VOCAB_SIZE, NUM_LAYERS, DROPOUT)
+    decoder = Decoder(VOCAB_SIZE, EMBEDDING_DIM, HIDDEN_SIZE, VOCAB_SIZE, NUM_LAYERS, DROPOUT, attention)
     model = Seq2Seq(encoder, decoder).to(device)
+
+    def init_weights(m):
+        for name, param in m.named_parameters():
+            nn.init.uniform_(param.data, -0.08, 0.08)
+            
+    model.apply(init_weights)
 
     # Training
     criterion = nn.CrossEntropyLoss(ignore_index=word2idx["<PAD>"])
     optimizer = optim.Adam(model.parameters())
 
-    print("Starting model training...")
+    print("Starting model training (with Attention)...")
     for epoch in range(EPOCHS):
         loss = train_epoch(model, conversations, optimizer, criterion)
         if (epoch + 1) % 10 == 0:
             print(f'Epoch: {epoch+1:02} | Loss: {loss:.3f}')
     print("Training finished.")
-
